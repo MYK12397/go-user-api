0710c374414df9254cf692a19b1f5035fa384bd7
MYK12397 <yusufzai.12397@gmail.com>
2021-10-04 01:24:34 +0530

    feat: ADDED driver code

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os/signal"
 	"time"
 
+	"github.com/MYK12397/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -16,10 +17,20 @@ func main() {
 	sm := mux.NewRouter()
 
 	getR := sm.Methods(http.MethodGet).Subrouter()
-	getR.HandleFunc("/v1/users")
+	getR.HandleFunc("/v1/users", handlers.GetUsers)
+	getR.HandleFunc("/v1/users/{id}", handlers.GetID)
+
+	postR := sm.Methods(http.MethodPost).Subrouter()
+	postR.HandleFunc("/v1/users", handlers.CreateUser)
+
+	putR := sm.Methods(http.MethodPut).Subrouter()
+	putR.HandleFunc("/v1/users/{id}", handlers.UpdateUser)
+
+	deleteR := sm.Methods(http.MethodDelete).Subrouter()
+	deleteR.HandleFunc("/v1/users/{id}", handlers.DeleteUser)
 
 	s := &http.Server{
-		Addr:         ":9090",
+		Addr:         ":8000",
 		Handler:      sm,
 		IdleTimeout:  120 * time.Second,
 		ReadTimeout:  5 * time.Second,
@@ -34,10 +45,10 @@ func main() {
 
 	}()
 
-	sigchan := make(chan os.Signal)
-	signal.Notify(sigchan, os.Interrupt)
-	signal.Notify(sigchan, os.Kill)
-	sig := <-sigchan
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+	sig := <-sigChan
 
 	fmt.Println("Received Terminal signal. Shutting down. ", sig)
 	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
