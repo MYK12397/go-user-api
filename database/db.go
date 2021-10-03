e09a5737006f5507c6765802f3cad453a7d167e8
MYK12397 <yusufzai.12397@gmail.com>
2021-10-04 00:30:44 +0530

    feat:ADDED mongo database

---
+++ database/db.go
@@ -0,0 +1,45 @@
+package database
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectDB() *mongo.Collection {
+
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("DataBase is Connected")
+
+	collection := client.Database("User1").Collection("data_details")
+
+	return collection
+}
+
+type ErrorResponse struct {
+	Code    int    `json:"status"`
+	Message string `json:"message"`
+}
+
+func GetError(err error, rw http.ResponseWriter) {
+
+	var res = ErrorResponse{
+		Code:    http.StatusInternalServerError,
+		Message: err.Error(),
+	}
+
+	msg, _ := json.Marshal(res)
+	rw.WriteHeader(res.Code)
+	rw.Write(msg)
+}
